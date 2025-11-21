d9d79ba5b3a8500055ff1df14afd787c08291a7a
Ghassan <77386661+GhassanAlKaraan@users.noreply.github.com>
2025-11-21 17:06:07 +0200

    Fix AppDelegate methods for Xcode 15/16

--- facebook_auth/ios/Classes/SwiftFlutterFacebookAuthPlugin.swift
+++ facebook_auth/ios/Classes/SwiftFlutterFacebookAuthPlugin.swift
@@ -1,42 +1,96 @@
+// import Flutter
+// import UIKit
+
+// import FBSDKCoreKit
+
+
+// public class SwiftFlutterFacebookAuthPlugin: NSObject, FlutterPlugin {
+//     let facebookAuth = FacebookAuth()
+//     public static func register(with registrar: FlutterPluginRegistrar) {
+//         ApplicationDelegate.initialize()
+//         let channel = FlutterMethodChannel(name: "app.meedu/flutter_facebook_auth", binaryMessenger: registrar.messenger())
+//         let instance = SwiftFlutterFacebookAuthPlugin()
+//         registrar.addMethodCallDelegate(instance, channel: channel)
+//         registrar.addApplicationDelegate(instance)
+//     }
+    
+//     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+//         self.facebookAuth.handle(call, result: result)
+//     }
+
+    
+    
+//     /// START ALLOW HANDLE NATIVE FACEBOOK APP
+//     public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any] = [:]) -> Bool {
+//         var options = [UIApplication.LaunchOptionsKey: Any]()
+//         for (k, value) in launchOptions {
+//             let key = k as! UIApplication.LaunchOptionsKey
+//             options[key] = value
+//         }
+//         ApplicationDelegate.shared.application(application,didFinishLaunchingWithOptions: options)
+//         return true
+//     }
+    
+//     public func application( _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool {
+//         let processed = ApplicationDelegate.shared.application(
+//             app, open: url,
+//             sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+//             annotation: options[UIApplication.OpenURLOptionsKey.annotation])
+//         return processed;
+//     }
+//     /// END ALLOW HANDLE NATIVE FACEBOOK APP
+// }
 import Flutter
 import UIKit
-
 import FBSDKCoreKit
-
 
 public class SwiftFlutterFacebookAuthPlugin: NSObject, FlutterPlugin {
     let facebookAuth = FacebookAuth()
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         ApplicationDelegate.initialize()
-        let channel = FlutterMethodChannel(name: "app.meedu/flutter_facebook_auth", binaryMessenger: registrar.messenger())
+        let channel = FlutterMethodChannel(
+            name: "app.meedu/flutter_facebook_auth",
+            binaryMessenger: registrar.messenger()
+        )
         let instance = SwiftFlutterFacebookAuthPlugin()
         registrar.addMethodCallDelegate(instance, channel: channel)
         registrar.addApplicationDelegate(instance)
     }
-    
+
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         self.facebookAuth.handle(call, result: result)
     }
 
-    
-    
-    /// START ALLOW HANDLE NATIVE FACEBOOK APP
-    public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any] = [:]) -> Bool {
-        var options = [UIApplication.LaunchOptionsKey: Any]()
-        for (k, value) in launchOptions {
-            let key = k as! UIApplication.LaunchOptionsKey
-            options[key] = value
-        }
-        ApplicationDelegate.shared.application(application,didFinishLaunchingWithOptions: options)
-        return true
+    // ------------------------------------------------------------
+    // FIXED: Updated for Xcode 15/16 / iOS 17+ SDK
+    // Apple removed didFinishLaunchingWithOptions for plugins.
+    // ------------------------------------------------------------
+
+    @objc(application:continueUserActivity:restorationHandler:)
+    public func application(
+        _ application: UIApplication,
+        continue userActivity: NSUserActivity,
+        restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+    ) -> Bool {
+        return ApplicationDelegate.shared.application(
+            application,
+            continue: userActivity,
+            restorationHandler: restorationHandler
+        )
     }
-    
-    public func application( _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool {
-        let processed = ApplicationDelegate.shared.application(
-            app, open: url,
-            sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
-            annotation: options[UIApplication.OpenURLOptionsKey.annotation])
-        return processed;
+
+    @objc(application:openURL:options:)
+    public func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        return ApplicationDelegate.shared.application(
+            app,
+            open: url,
+            sourceApplication: options[.sourceApplication] as? String,
+            annotation: options[.annotation]
+        )
     }
-    /// END ALLOW HANDLE NATIVE FACEBOOK APP
 }
